e559a48c603c5810e700c963be0878e541d6c0b2
marion <m.craipeau@studio-hb.com>
2017-08-26 14:48:49 +0200

    up db

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+DATABASE_URL="postgres://hamilhyhvehudz:389abb47b7d444dd9875c37c50ce357ca170aa7b1522a928299837363a852e50@ec2-107-22-251-55.compute-1.amazonaws.com:5432/dcafn4teq8135"
